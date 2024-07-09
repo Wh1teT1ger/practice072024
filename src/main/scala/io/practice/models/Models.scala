40f468198e98af5f54495e28ca781bee31902a45
denisburylov <burylov01@mail.ru>
2024-07-09 12:47:10 +0500

    add ObjectId in Page and Report

--- src/main/scala/io/practice/models/Models.scala
+++ src/main/scala/io/practice/models/Models.scala
@@ -1,7 +1,8 @@
 package io.practice.models
 
 import org.bson.codecs.pojo.annotations.BsonProperty
+import org.mongodb.scala.bson.ObjectId
 
-case class Page(@BsonProperty("_id") id: String, content: String, projectId: String)
+case class Page( _id: ObjectId, content: String, projectId: String)
 
-case class Report(@BsonProperty("_id") id: String, stopWords: List[String], projectId: String, pageId: String, addedAt: Long = System.currentTimeMillis())
+case class Report( _id: ObjectId, stopWords: List[String], projectId: String, pageId: String, addedAt: Long = System.currentTimeMillis())
